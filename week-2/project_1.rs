70e531ad31691bfb132aaea66f7f64b84cf61c68
jareadesokan <adejare.adesokan@pau.edu.ng>
2024-10-23 19:08:56 +0100

    (My second commit)

--- week-2/project_1.rs
+++ week-2/project_1.rs
@@ -1,10 +1,13 @@
 fn main() {
-	let p:f64 = 520000000.0;
-	let r:f64 = 10.0;
-	let n:i32 = 5;
+    let p: f64 = 520000000.0; 
+    let t: i32 = 5;
+    let r: f64 = 10.0; 
 
-	let a = p * (1.0 + (r / 100.0)).powi(n);
-	
-	let ci = a - p;
-    println!("Compound Interest is{}",ci);
+    // Calculate amount using the formula A = P * (1 + r/n)^(nt)
+    
+    let a = p * (1.0 + (r / 100.0)).powi(t);
+    let ci = a - p;
+    
+    //The result of the compound interest
+    println!("The Compound Interest is {}", ci);
 }
